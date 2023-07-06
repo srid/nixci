8d35988fa7a07f9919f687fd77dc4d26d0296582
Sridhar Ratnakumar <srid@srid.ca>
2023-07-06 11:09:06 -0400

    Run devour-flake on argument

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use argh::FromArgs;
+use std::io;
+use std::process::Command;
 
 #[derive(FromArgs, Debug)]
 /// Application configuration
@@ -7,15 +9,19 @@ struct Config {
     #[argh(switch, short = 'v')]
     verbose: bool,
 
-    /// an optional name to green
-    #[argh(option)]
-    name: Option<String>,
+    /// flake URL or github URL
+    #[argh(positional, default = r#" ".".to_string() "#)]
+    url: String,
 }
 
-fn main() {
-    let cfg: Config = argh::from_env();
+fn main() -> io::Result<()> {
+    let cfg = argh::from_env::<Config>();
     if cfg.verbose {
         println!("DEBUG {cfg:?}");
     }
-    println!("Hello {}!", cfg.name.unwrap_or("world".to_string()));
+    println!("Running nixci on {}", cfg.url.to_string());
+    let c = Command::new("devour-flake").arg(cfg.url).spawn()?;
+    let output = c.wait_with_output()?;
+    println!("\noutput: {:?}", output);
+    Ok(())
 }
