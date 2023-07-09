4396ed52cd6d0d90720da957384a5a5fad87a20e
Sridhar Ratnakumar <srid@srid.ca>
2023-07-08 21:36:47 -0400

    Comments & TODO

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use argh::FromArgs;
 use std::process::{Command, Stdio};
 
-const CURRENT_FLAKE: &str = ".";
+const CURRENT_FLAKE_URL: &str = ".";
 
+/// Absolute path to the devour-flake executable
+///
+/// We expect this environment to be set in Nix build and shell.
 const DEVOUR_FLAKE: &str = env!("DEVOUR_FLAKE");
 
 #[derive(FromArgs, Debug)]
@@ -13,7 +16,7 @@ struct Config {
     verbose: bool,
 
     /// flake URL or github URL
-    #[argh(positional, default = "CURRENT_FLAKE.to_string()")]
+    #[argh(positional, default = "CURRENT_FLAKE_URL.to_string()")]
     url: String,
 }
 
@@ -25,13 +28,16 @@ fn main() -> AppResult<()> {
     if cfg.verbose {
         println!("DEBUG {cfg:?}");
     }
+    // TODO: Handle github urls, in particular PR urls
     println!("Running nixci on {}", cfg.url.to_string());
+    // TODO: Run `nix flake lock --no-update-lock-file` and report on it.
     let output = Command::new(DEVOUR_FLAKE)
         .arg(cfg.url)
         .stdout(Stdio::piped())
         .spawn()?
         .wait_with_output()?;
     if output.status.success() {
+        // TODO: Strip devour-flake's follows-logging output
         let raw_output = String::from_utf8(output.stdout)?;
         let outs = raw_output.split_ascii_whitespace();
         if outs.clone().count() == 0 {
