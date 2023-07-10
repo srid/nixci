dac8a7f5005630e1f8e37963fd7056e80945a5c6
Sridhar Ratnakumar <srid@srid.ca>
2023-07-10 17:38:33 -0400

    Decouple further

--- src/devour_flake.rs
+++ src/devour_flake.rs
@@ -1,17 +1,39 @@
+use crate::app::AppResult;
 /// Rust support for invoking https://github.com/srid/devour-flake
-use std::process::{Command, Stdio, Output};
-use crate::app::{AppResult};
+use std::{process::{Command, Stdio}};
 
 /// Absolute path to the devour-flake executable
 ///
 /// We expect this environment to be set in Nix build and shell.
 const DEVOUR_FLAKE: &str = env!("DEVOUR_FLAKE");
 
-pub fn devour_flake(url: String) -> AppResult<Output> {
-    let out = Command::new(DEVOUR_FLAKE)
+pub type DrvOut = String;
+
+pub fn devour_flake(url: String) -> AppResult<Vec<DrvOut>> {
+    // TODO: Strip devour-flake's follows-logging output
+    let output = Command::new(DEVOUR_FLAKE)
         .arg(url)
         .stdout(Stdio::piped())
         .spawn()?
         .wait_with_output()?;
-    Ok(out)
+    if output.status.success() {
+        parse_devour_flake_output(output.stdout)
+    } else {
+        println!("ERROR: devour-flake failed");
+        std::process::exit(output.status.code().unwrap_or(1));
+    }
+}
+
+/// Parse stdout of `devour-flake` executable
+///
+/// It spits out drv outs built separated by whitespace.
+fn parse_devour_flake_output(stdout: Vec<u8>) -> AppResult<Vec<DrvOut>> {
+    let raw_output = String::from_utf8(stdout)?;
+    let outs = raw_output.split_ascii_whitespace();
+    if outs.clone().count() == 0 {
+        println!("ERROR: No outputs produced by devour-flake");
+        std::process::exit(1);
+    } else {
+        Ok(outs.map(|s| s.to_string()).collect())
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 mod app;
 mod devour_flake;
 
-
 fn main() -> app::AppResult<()> {
     // TODO: Subflake support: parse `.envrc`? Or `nixci.json`?
     let cfg = argh::from_env::<app::Config>();
@@ -11,20 +10,14 @@ fn main() -> app::AppResult<()> {
     // TODO: Handle github urls, in particular PR urls
     println!("Running nixci on {}", cfg.url.to_string());
     // TODO: Run `nix flake lock --no-update-lock-file` and report on it.
-    let output = devour_flake::devour_flake(cfg.url)?;
-    if output.status.success() {
-        // TODO: Strip devour-flake's follows-logging output
-        let raw_output = String::from_utf8(output.stdout)?;
-        let outs = raw_output.split_ascii_whitespace();
-        if outs.clone().count() == 0 {
-            println!("ERROR: No outputs produced by devour-flake");
-            std::process::exit(1);
-        } else {
-            outs.for_each(|out| println!("out: {}", out));
-            Ok(())
-        }
+    let outs = devour_flake::devour_flake(cfg.url)?;
+    if outs.len() == 0 {
+        println!("ERROR: No outputs produced by devour-flake");
+        std::process::exit(1);
     } else {
-        println!("ERROR: devour-flake failed");
-        std::process::exit(output.status.code().unwrap_or(1));
+        for out in outs {
+            println!("out: {}", out);
+        }
+        Ok(())
     }
 }
