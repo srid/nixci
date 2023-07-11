a3771d8f8eec61564faa7691d772244a2a859e95
Sridhar Ratnakumar <srid@srid.ca>
2023-07-11 15:34:14 -0400

    Colorize

--- src/main.rs
+++ src/main.rs
@@ -11,19 +11,19 @@ fn main() -> Result<()> {
         println!("DEBUG {args:?}");
     }
     // TODO: Handle github urls, in particular PR urls
-    println!("Running nixci on {}", args.url.to_string());
-
     // TODO: Run `nix flake lock --no-update-lock-file` and report on it.
 
     let cfgs = config::Config::from_flake_url(args.url.clone())?;
     if args.verbose {
         println!("DEBUG {cfgs:?}");
     }
-    for (cfg_name, cfg) in &cfgs.0 {
+    for (_cfg_name, cfg) in &cfgs.0 {
         let nix_args = cfg.nix_build_args_for_flake(args.url.clone());
         println!(
             "{}",
-            format!("> FLAKE[{}] devour-flake {}", cfg_name, nix_args.join(" ")).blue()
+            format!("❄️ devour-flake {}", nix_args.join(" "))
+                .blue()
+                .bold()
         );
 
         let outs = nix::devour_flake::devour_flake(nix_args)?;
