e9e3d8f19c1c22d2025f09d5d7f25930039a6a6f
Ivan Petkov <ivanppetkov@gmail.com>
2023-08-29 18:03:00 -0700

    Bring back format!

--- src/nix/util.rs
+++ src/nix/util.rs
@@ -1,19 +1,20 @@
 use colored::Colorize;
-use std::io::{stderr, BufWriter, Write};
 
 /// Print a shell command that the user can run as-is.
 pub fn print_shell_command<'a>(program: &str, args: impl Iterator<Item = &'a str>) {
-    let mut stderr = BufWriter::new(stderr().lock());
-
     let color = |s: &str| s.blue().bold();
-
-    let _ = write!(stderr, "> {}", color(program));
-
-    for arg in args {
-        let quote = color(if arg.contains('?') { "\"" } else { "" });
-        let _ = write!(stderr, " {quote}{}{quote}", color(arg));
-    }
-
-    let _ = writeln!(stderr);
-    let _ = stderr.flush();
+    println!(
+        "> {} {}",
+        color(program),
+        color(
+            &args
+                .map(|x| if x.contains('?') {
+                    format!("\"{}\"", x)
+                } else {
+                    x.to_string()
+                })
+                .collect::<Vec<_>>()
+                .join("_")
+        )
+    );
 }
