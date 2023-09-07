372837ceda53b30f11c52fbfc83c8c420b0b1489
Sridhar Ratnakumar <srid@srid.ca>
2023-09-07 13:29:56 -0400

    Add library, and update clap

---
+++ src/lib.rs
@@ -0,0 +1,43 @@
+pub mod cli;
+pub mod config;
+pub mod github;
+pub mod nix;
+
+use anyhow::bail;
+use cli::CliArgs;
+use colored::Colorize;
+use nix::devour_flake::DrvOut;
+
+pub fn nixci(args: CliArgs) -> anyhow::Result<Vec<DrvOut>> {
+    if args.verbose {
+        eprintln!("DEBUG {args:?}");
+    }
+    let url = args.flake_ref.to_flake_url()?;
+    eprintln!("{}", format!("🍏 {}", url.0).bold());
+
+    let ((cfg_name, cfg), url) = config::Config::from_flake_url(&url)?;
+    if args.verbose {
+        eprintln!("DEBUG {cfg:?}");
+    }
+
+    let mut all_outs = vec![];
+
+    for (subflake_name, subflake) in &cfg.0 {
+        let nix_args = subflake.nix_build_args_for_flake(&args, &url);
+        eprintln!("🍎 {}", format!("{}.{}", cfg_name, subflake_name).italic());
+        if subflake.override_inputs.is_empty() {
+            nix::lock::nix_flake_lock_check(&url.sub_flake_url(subflake.dir.clone()))?;
+        }
+
+        let outs = nix::devour_flake::devour_flake(args.verbose, nix_args)?;
+        if outs.is_empty() {
+            bail!("Warn: devour-flake produced no outputs");
+        } else {
+            for out in &outs {
+                println!("{}", out.0.bold());
+            }
+            all_outs.extend(outs);
+        }
+    }
+    Ok(all_outs)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,9 @@
-mod cli;
-mod config;
-mod github;
-mod nix;
-
 use anyhow::Result;
 use clap::Parser;
-use colored::Colorize;
+use nixci::cli;
 
 fn main() -> Result<()> {
     let args = cli::CliArgs::parse();
-    if args.verbose {
-        eprintln!("DEBUG {args:?}");
-    }
-    let url = args.flake_ref.to_flake_url()?;
-    eprintln!("{}", format!("🍏 {}", url.0).bold());
-
-    let ((cfg_name, cfg), url) = config::Config::from_flake_url(&url)?;
-    if args.verbose {
-        eprintln!("DEBUG {cfg:?}");
-    }
-
-    for (subflake_name, subflake) in &cfg.0 {
-        let nix_args = subflake.nix_build_args_for_flake(&args, &url);
-        eprintln!("🍎 {}", format!("{}.{}", cfg_name, subflake_name).italic());
-        if subflake.override_inputs.is_empty() {
-            nix::lock::nix_flake_lock_check(&url.sub_flake_url(subflake.dir.clone()))?;
-        }
-
-        let outs = nix::devour_flake::devour_flake(args.verbose, nix_args)?;
-        if outs.is_empty() {
-            eprintln!("Warn: devour-flake produced no outputs")
-        } else {
-            for out in outs {
-                println!("{}", out.0.bold());
-            }
-        }
-    }
+    let _outs = nixci::nixci(args)?;
     Ok(())
 }
