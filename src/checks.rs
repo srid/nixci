90d9d478b6e5f9bcddcc356fa9b6644a5e295ca2
Sridhar Ratnakumar <srid@srid.ca>
2024-07-10 17:01:05 -0400

    refactor: be explicit, not implicit

--- src/checks.rs
+++ src/checks.rs
@@ -5,14 +5,11 @@ use nix_health::NixHealth;
 use nix_rs::{flake::url::FlakeUrl, info::NixInfo};
 
 pub async fn run_checks(flake_url: &FlakeUrl, nix_info: &NixInfo) -> anyhow::Result<()> {
-    // Works only on `nix-health.default` configuration. To use a different configuration, modify the flake_url to <flake_url>#<attr>
-    let nix_health_url = flake_url.with_fully_qualified_root_attr("nix-health");
+    let nix_health = NixHealth::from_flake(flake_url).await?;
 
-    let nix_health = NixHealth::from_flake(nix_health_url.clone()).await?;
-
-    let items: Vec<&dyn Checkable> = vec![&nix_health.nix_version];
-
-    let checks = NixHealth::run_checks_with(items.into_iter(), nix_info, Some(nix_health_url));
+    let checks = nix_health
+        .nix_version
+        .check(nix_info, Some(flake_url.clone()));
 
     let exit_code = NixHealth::print_report_returning_exit_code(&checks, false);
 
