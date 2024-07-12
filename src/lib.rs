069501babbeee1dd6efcd35ec850664167127264
Rohit Singh <rsrohitsingh682@gmail.com>
2024-07-12 23:11:10 +0530

    fix: clippy warning

--- src/lib.rs
+++ src/lib.rs
@@ -142,7 +142,7 @@ async fn nixci_subflake(
 
 pub async fn check_nix_version(flake_url: &FlakeUrl, nix_info: &NixInfo) -> anyhow::Result<()> {
     let nix_health = NixHealth::from_flake(flake_url).await?;
-    let checks = nix_health.nix_version.check(&nix_info, Some(flake_url));
+    let checks = nix_health.nix_version.check(nix_info, Some(flake_url));
     let exit_code = NixHealth::print_report_returning_exit_code(&checks);
 
     if exit_code != 0 {
